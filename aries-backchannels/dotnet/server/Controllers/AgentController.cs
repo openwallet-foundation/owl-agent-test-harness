c5c32848ab6b7a25b7b949fdfa0ff112464117f1
Timo Glastra <timo@animo.id>
2020-06-30 22:29:54 +0200

    dotnet: add initial backchannel setup
    Signed-off-by: Timo Glastra <timo@animo.id>

---
+++ aries-backchannels/dotnet/server/Controllers/AgentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet.Backchannel.Controllers
+{
+    [Route("agent/command")]
+    [ApiController]
+    public class AgentController : ControllerBase
+    {
+        [HttpGet("status")]
+        public IActionResult AgentStatus()
+        {
+            // TODO: return status 418 if not active
+            return StatusCode(200);
+        }
+    }
+}
---
+++ aries-backchannels/dotnet/server/Controllers/ConnectionController.cs
@@ -0,0 +1,97 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using DotNet.Backchannel.Models;
+
+using Hyperledger.Aries.Features.DidExchange;
+using Hyperledger.Aries.Features.TrustPing;
+using Hyperledger.Aries.Agents;
+
+namespace DotNet.Backchannel.Controllers
+{
+    [Route("agent/command/connection")]
+    [ApiController]
+    public class ConnectionController : ControllerBase
+    {
+        private readonly IConnectionService _connectionService;
+        private readonly IAgentProvider _agentContextProvider;
+        private readonly IMessageService _messageService;
+
+        public ConnectionController(
+            IConnectionService connectionService,
+            IAgentProvider agentContextProvider,
+            IMessageService messageService)
+        {
+            _connectionService = connectionService;
+            _agentContextProvider = agentContextProvider;
+            _messageService = messageService;
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ConnectionOperationAsync(OperationBody body)
+        {
+            switch (body.Operation)
+            {
+                case "create-invitation":
+                    return await this.CreateInvitationAsync();
+                case "receive-invitation":
+                    return await this.ReceiveInvitationAsync(body.Data);
+                case "accept-invitation":
+                    return await this.AcceptInvitationAsync(body.Id);
+                case "accept-request":
+                    return await this.AcceptRequestAsync(body.Id);
+                case "send-ping":
+                    return await this.SendPingAsync(body.Id, body.Data);
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        private async Task<IActionResult> CreateInvitationAsync()
+        {
+            var context = await _agentContextProvider.GetContextAsync();
+
+            var (invitation, connection) = await _connectionService.CreateInvitationAsync(context, new InviteConfiguration());
+
+            return StatusCode(200, new { connection_id = connection.Id, invitation });
+        }
+
+        private async Task<IActionResult> ReceiveInvitationAsync(object invitation)
+        {
+            throw new NotImplementedException();
+            // var context = await _agentContextProvider.GetContextAsync();
+
+            // var invitation = new UnpackedMessageContext(body.Data.ToString(), senderVerkey: null).GetMessage<ConnectionInvitationMessage>();
+            // var (request, record) = await _connectionService.CreateRequestAsync(context, invitation);
+
+            // var response = await messageService.SendReceiveAsync<ConnectionResponseMessage>(context.Wallet, request, record);
+
+            // await connectionService.ProcessResponseAsync(agentContext, response, record);
+        }
+
+        private async Task<IActionResult> AcceptInvitationAsync(string connectionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task<IActionResult> AcceptRequestAsync(string connectionId)
+        {
+            throw new NotImplementedException();
+        }
+        private async Task<IActionResult> SendPingAsync(string connectionId, dynamic data)
+        {
+            var context = await _agentContextProvider.GetContextAsync();
+            var connection = await _connectionService.GetAsync(context, connectionId);
+            var message = new TrustPingMessage
+            {
+                ResponseRequested = true,
+                Comment = data.comment
+            };
+
+            await _messageService.SendAsync(context.Wallet, message, connection);
+
+            return StatusCode(200);
+        }
+    }
+}
