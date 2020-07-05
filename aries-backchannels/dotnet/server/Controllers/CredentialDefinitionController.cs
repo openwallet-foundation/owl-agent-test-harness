876b4f7c34583a1309b5fcd0eec1bc492fde8e24
Timo Glastra <timo@animo.id>
2020-07-05 21:09:58 +0200

    dotnet: add credential-definition operations
    Signed-off-by: Timo Glastra <timo@animo.id>

--- aries-backchannels/dotnet/server/Controllers/CredentialDefinitionController.cs
+++ aries-backchannels/dotnet/server/Controllers/CredentialDefinitionController.cs
@@ -1,7 +1,13 @@
-using System;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using DotNet.Backchannel.Models;
+
+using Hyperledger.Aries.Features.IssueCredential;
+using Hyperledger.Aries.Agents;
+using Hyperledger.Aries.Configuration;
+using System.Net.Mime;
+using Hyperledger.Indy;
+using System.Text.Json;
 
 namespace DotNet.Backchannel.Controllers
 {
@@ -9,10 +15,35 @@ namespace DotNet.Backchannel.Controllers
     [ApiController]
     public class CredentialDefinitionController : ControllerBase
     {
-        [HttpGet("{id}")]
+        private readonly IAgentProvider _agentContextProvider;
+        private readonly IProvisioningService _provisionService;
+        private readonly ISchemaService _schemaService;
+
+        public CredentialDefinitionController(
+            IAgentProvider agentContextProvider,
+            IProvisioningService provisionService,
+            ISchemaService schemaService)
+        {
+            _agentContextProvider = agentContextProvider;
+            _provisionService = provisionService;
+            _schemaService = schemaService;
+        }
+
+        [HttpGet("{credentialDefinitionId}")]
         public async Task<IActionResult> GetCredentialDefinitionByIdAsync([FromRoute] string credentialDefinitionId)
         {
-            throw new NotImplementedException();
+            var context = await _agentContextProvider.GetContextAsync();
+
+            try
+            {
+                var credentialDefinition = await _schemaService.LookupCredentialDefinitionAsync(context, credentialDefinitionId);
+                return Content(credentialDefinition, MediaTypeNames.Application.Json);
+            }
+            catch (IndyException indyException)
+            {
+                if (indyException.SdkErrorCode == 309) return NotFound();
+                else return StatusCode(500);
+            }
         }
 
         [HttpPost]
@@ -22,9 +53,23 @@ namespace DotNet.Backchannel.Controllers
             return await this.CreateCredentialDefinitionAsync(body.Data);
         }
 
-        private async Task<IActionResult> CreateCredentialDefinitionAsync(dynamic credentialDefinition)
+        private async Task<IActionResult> CreateCredentialDefinitionAsync(JsonElement credentialDefinition)
         {
-            throw new NotImplementedException();
+            var context = await _agentContextProvider.GetContextAsync();
+            var issuer = await _provisionService.GetProvisioningAsync(context.Wallet);
+
+            var credentialDefinitionId = await _schemaService.CreateCredentialDefinitionAsync(context, new CredentialDefinitionConfiguration
+            {
+                SchemaId = credentialDefinition.GetProperty("schema_id").GetString(),
+                Tag = credentialDefinition.GetProperty("tag").GetString(),
+                EnableRevocation = credentialDefinition.GetProperty("support_revocation").GetBoolean(),
+                IssuerDid = issuer.IssuerDid
+            });
+
+            return Ok(new
+            {
+                credential_definition_id = credentialDefinitionId
+            });
         }
     }
 }
