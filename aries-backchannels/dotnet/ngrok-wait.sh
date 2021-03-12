a9e4c61cd79bb3a3d245d37d22df27f7258a2376
Ian Costanzo <ian@anon-solutions.ca>
2021-03-12 11:36:43 -0800

    Added ngrok endpoint support to dotnet-master
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

---
+++ aries-backchannels/dotnet/ngrok-wait.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# based on code developed by Sovrin:  https://github.com/hyperledger/aries-acapy-plugin-toolbox
+
+echo "using ngrok end point [$NGROK_NAME]"
+
+NGROK_ENDPOINT=null
+while [ -z "$NGROK_ENDPOINT" ] || [ "$NGROK_ENDPOINT" = "null" ]
+do
+    echo "Fetching end point from ngrok service"
+    NGROK_ENDPOINT=$(curl --silent $NGROK_NAME:4040/api/tunnels | ./jq -r '.tunnels[] | select(.proto=="https") | .public_url')
+
+    if [ -z "$NGROK_ENDPOINT" ] || [ "$NGROK_ENDPOINT" = "null" ]; then
+        echo "ngrok not ready, sleeping 5 seconds...."
+        sleep 5
+    fi
+done
+
+export AGENT_PUBLIC_ENDPOINT=$NGROK_ENDPOINT
+
+echo "fetched end point [$AGENT_PUBLIC_ENDPOINT]"
+
+echo "Starting dotnet agent ..."
+
+dotnet DotNet.Backchannel.Master.dll "$@"
