nodlesh/aries-agent-test-harness

Credo Backchannel & Dev Container
Signed-off-by: Sheldon Regular <sheldon.regular@gmail.com>

aries-backchannels/credo-ts/server/src/controllers/CredentialDefinitionController.ts
@@ -1,0 +1,101 @@
+import { Controller, Get, PathParams, Post, BodyParams } from '@tsed/common'
+import { InternalServerError, NotFound } from '@tsed/exceptions'
+import { BaseController } from '../BaseController'
+import { TestHarnessConfig } from '../TestHarnessConfig'
+import { AnonCredsApi, AnonCredsCredentialDefinition, AnonCredsCredentialDefinitionRepository } from '@credo-ts/anoncreds'
+import { DidInfo } from '../types'
+
+@Controller('/agent/command/credential-definition')
+export class CredentialDefinitionController extends BaseController {
+  public constructor(testHarnessConfig: TestHarnessConfig) {
+    super(testHarnessConfig)
+  }
+
+  @Get('/:credentialDefinitionId')
+  async getCredentialDefinitionById(
+    @PathParams('credentialDefinitionId') credentialDefinitionId: string
+  ): Promise<ReturnedCredentialDefinition> {
+    try {
+      const { credentialDefinition } = await this.agent.modules.anoncreds.getCredentialDefinition(credentialDefinitionId)
+
+      if (!credentialDefinition) {
+        throw new NotFound(`credential definition with credentialDefinitionId "${credentialDefinitionId}" not found.`)
+      }
+      return { ...credentialDefinition, id: credentialDefinitionId }
+    } catch (error) {
+      // Credential definition does not exist on ledger
+      if (error instanceof NotFound) {
+        throw error
+      }
+
+      // All other errors
+      throw new InternalServerError(
+        `Error while retrieving credential definition with id ${credentialDefinitionId}`,
+        error
+      )
+    }
+  }
+
+  @Post()
+  async createCredentialDefinition(
+    @BodyParams('data')
+    data: {
+      tag: string
+      support_revocation: boolean
+      schema_id: string
+    }
+  ): Promise<{
+    credential_definition_id: string
+    credential_definition: ReturnedCredentialDefinition
+  }> {
+
+    // Check locally if credential definition already exists
+    const credentialDefinitionRepository = this.agent.dependencyManager.resolve(AnonCredsCredentialDefinitionRepository)
+    const [credentialDefinitionRecord] = await credentialDefinitionRepository.findByQuery(this.agent.context, { schemaId: data.schema_id, tag: data.tag })
+    if (credentialDefinitionRecord) {
+
+      return {
+        credential_definition_id: credentialDefinitionRecord.credentialDefinitionId,
+        credential_definition: { ...credentialDefinitionRecord.credentialDefinition, id: credentialDefinitionRecord.credentialDefinitionId },
+      }
+    }
+
+    // TODO: handle schema not found exception
+    try {
+
+      const anoncredsApi = this.agent.dependencyManager.resolve(AnonCredsApi)
+
+      const schema = await anoncredsApi.getSchema(data.schema_id)
+
+      const publicDidInfoRecord = await this.agent.genericRecords.findById('PUBLIC_DID_INFO')
+    
+      if (!publicDidInfoRecord) {
+        throw new Error('Agent does not have any public did')
+      }
+  
+      const issuerId = (publicDidInfoRecord.content.didInfo as unknown as DidInfo).did
+      
+      const { credentialDefinitionState } = await anoncredsApi.registerCredentialDefinition({ 
+        credentialDefinition: {
+          issuerId,
+          schemaId: schema.schemaId,
+          tag: data.tag,
+        }, options: { supportRevocation: false, didIndyNamespace: 'main-pool'}}) 
+
+      if (!credentialDefinitionState.credentialDefinition || !credentialDefinitionState.credentialDefinitionId) {
+        throw new Error()
+      }
+      
+      return {
+        credential_definition_id: credentialDefinitionState.credentialDefinitionId,
+        credential_definition: { ...credentialDefinitionState.credentialDefinition, id: credentialDefinitionState.credentialDefinitionId },
+      }
+    } catch (error: any) {
+      throw new InternalServerError(`Error registering credential definition: ${error.message}`, error)
+    }
+  }
+}
+
+interface ReturnedCredentialDefinition extends AnonCredsCredentialDefinition {
+  id: string
+}

aries-backchannels/credo-ts/server/src/controllers/OutOfBandController.ts
@@ -1,0 +1,169 @@
+import { Controller, Get, PathParams, Post, BodyParams } from '@tsed/common'
+import { NotFound } from '@tsed/exceptions'
+import {
+  OutOfBandRecord,
+  OutOfBandInvitation,
+  ConnectionInvitationMessage,
+  ConnectionRecord,
+  JsonTransformer,
+  AgentConfig,
+  OutOfBandEventTypes,
+  OutOfBandStateChangedEvent,
+  OutOfBandState,
+  CredoError,
+  DidExchangeState,
+} from '@credo-ts/core'
+
+import { convertToNewInvitation } from '@credo-ts/core/build/modules/oob/helpers'
+import { filter, firstValueFrom, ReplaySubject, tap, timeout } from 'rxjs'
+import { BaseController } from '../BaseController'
+import { TestHarnessConfig } from '../TestHarnessConfig'
+import { ConnectionUtils } from '../utils/ConnectionUtils'
+
+@Controller('/agent/command/oob')
+export class OutOfBandController extends BaseController {
+  private subject = new ReplaySubject<OutOfBandStateChangedEvent>()
+
+  public constructor(testHarnessConfig: TestHarnessConfig) {
+    super(testHarnessConfig)
+  }
+
+  onStartup = () => {
+    this.subject = new ReplaySubject<OutOfBandStateChangedEvent>()
+    // Catch all events in replay subject for later use
+    this.agent.events
+      .observable<OutOfBandStateChangedEvent>(OutOfBandEventTypes.OutOfBandStateChanged)
+      .subscribe(this.subject)
+  }
+
+  @Get('/:id')
+  async getConnectionById(@PathParams('id') id: string) {
+    const connection = await ConnectionUtils.getConnectionByConnectionIdOrOutOfBandId(this.agent, id)
+
+    if (!connection) throw new NotFound(`Connection with id ${id} not found`)
+
+    return this.mapConnection(connection)
+  }
+
+  @Get('/')
+  async getAllConnections() {
+    const connections = await this.agent.connections.getAll()
+
+    return connections.map((conn) => this.mapConnection(conn))
+  }
+
+  @Post('/create-invitation')
+  async createInvitation(@BodyParams('data') data?: { mediator_connection_id?: string }) {
+    const mediatorId = await this.mediatorIdFromMediatorConnectionId(data?.mediator_connection_id)
+
+    const routing = await this.agent.mediationRecipient.getRouting({
+      mediatorId,
+    })
+
+    const outOfBandRecord = await this.agent.oob.createInvitation({
+      routing,
+      // Needed to complete connection: https://github.com/hyperledger/aries-framework-javascript/issues/668
+      // TODO probably don't need this anymore.
+      autoAcceptConnection: true,
+    })
+
+    const config = this.agent.dependencyManager.resolve(AgentConfig)
+    const invitation = outOfBandRecord.outOfBandInvitation
+    const invitationJson = invitation.toJSON({ useDidSovPrefixWhereAllowed: config.useDidSovPrefixWhereAllowed })
+
+    return {
+      state: OutOfBandState.Initial,
+      // This should be the connection id. However, the connection id is not available until a request is received.
+      // We can just use the oob invitation I guess.
+      connection_id: outOfBandRecord.id,
+      invitation: invitationJson,
+    }
+  }
+
+  @Post('/receive-invitation')
+  async receiveInvitation(@BodyParams('data') data: Record<string, unknown> & { mediator_connection_id?: string }) {
+    const { mediator_connection_id, ...invitationJson } = data
+
+    const mediatorId = await this.mediatorIdFromMediatorConnectionId(mediator_connection_id)
+
+    const routing = await this.agent.mediationRecipient.getRouting({
+      mediatorId,
+    })
+
+    const oobInvitation = convertToNewInvitation(JsonTransformer.fromJSON(invitationJson, ConnectionInvitationMessage))
+    const { outOfBandRecord, connectionRecord } = await this.agent.oob.receiveInvitation(oobInvitation, {
+      routing,
+      // Needed to complete connection: https://github.com/hyperledger/aries-framework-javascript/issues/668
+      autoAcceptConnection: true,
+      autoAcceptInvitation: true,
+    })
+
+    this.agent.config.logger.debug('OutOfBandController.receiveInvitation: outOfBandRecord.id: ', outOfBandRecord)
+
+    if (!connectionRecord) {
+      throw new CredoError('Processing invitation did not result in a out of band record')
+    }
+
+    return this.mapConnection(outOfBandRecord)
+  }
+
+  @Post('/accept-invitation')
+  async acceptInvitation(@BodyParams('id') id: string) {
+    const connection = await ConnectionUtils.getConnectionByConnectionIdOrOutOfBandId(this.agent, id)
+
+    return this.mapConnection(connection)
+  }
+
+  @Post('/accept-request')
+  async acceptRequest(@BodyParams('id') id: string) {
+    // possible here that the request hasn't finished processing yet
+    await this.waitForState(id, DidExchangeState.RequestReceived)
+
+    const connection = await ConnectionUtils.getConnectionByConnectionIdOrOutOfBandId(this.agent, id)
+
+    return this.mapConnection(connection)
+  }
+
+  @Post('/send-ping')
+  async sendPing(
+    @BodyParams('id') id: string,
+    // For now we ignore data. This could contain a comment property,
+    // AFJ doesn't support passing it for now
+    @BodyParams('data') data: any
+  ) {
+    const connection = await ConnectionUtils.getConnectionByConnectionIdOrOutOfBandId(this.agent, id)
+
+    if (!connection) throw new NotFound(`Connection with id ${id} not found`)
+
+    return this.mapConnection(connection)
+  }
+
+  private async mediatorIdFromMediatorConnectionId(mediatorConnectionId?: string): Promise<string | undefined> {
+    if (!mediatorConnectionId) return undefined
+
+    // Find mediator id if mediator connection id is provided
+    const mediator = await this.agent.mediationRecipient.findByConnectionId(mediatorConnectionId)
+
+    return mediator?.id
+  }
+
+  private async waitForState(id: string, state: DidExchangeState) {
+    return await firstValueFrom(
+      this.subject.pipe(
+        filter((c) => c.payload.outOfBandRecord.id === id ),
+        filter((c) => c.payload.outOfBandRecord.state === state),
+        timeout(20000)
+      )
+    )
+  }
+
+  private mapConnection(connection: ConnectionRecord) {
+    return {
+      // If we use auto accept, we can't include the state as we will move quicker than the calls in the test harness. This will
+      // make verification fail. The test harness recognizes the 'N/A' state.
+      state: connection.state === DidExchangeState.Completed ? connection.rfc0160State : 'N/A',
+      connection_id: connection.id,
+      connection,
+    }
+  }
+}

openwallet-foundation/owl-agent-test-harness

aries-backchannels/credo-ts/server/src/controllers/CredentialDefinitionController.ts
@@ -1,0 +1,105 @@
+import { Controller, Get, PathParams, Post, BodyParams } from '@tsed/common'
+import { InternalServerError, NotFound } from '@tsed/exceptions'
+import { BaseController } from '../BaseController'
+import { TestHarnessConfig } from '../TestHarnessConfig'
+import { AnonCredsApi, AnonCredsCredentialDefinition, AnonCredsCredentialDefinitionRepository } from '@credo-ts/anoncreds'
+import { DidInfo } from '../types'
+
+@Controller('/agent/command/credential-definition')
+export class CredentialDefinitionController extends BaseController {
+  public constructor(testHarnessConfig: TestHarnessConfig) {
+    super(testHarnessConfig)
+  }
+
+  @Get('/:credentialDefinitionId')
+  async getCredentialDefinitionById(
+    @PathParams('credentialDefinitionId') credentialDefinitionId: string
+  ): Promise<ReturnedCredentialDefinition> {
+    try {
+      const { credentialDefinition } = await this.agent.modules.anoncreds.getCredentialDefinition(credentialDefinitionId)
+
+      if (!credentialDefinition) {
+        throw new NotFound(`credential definition with credentialDefinitionId "${credentialDefinitionId}" not found.`)
+      }
+      return { ...credentialDefinition, id: credentialDefinitionId }
+    } catch (error) {
+      // Credential definition does not exist on ledger
+      if (error instanceof NotFound) {
+        throw error
+      }
+
+      // All other errors
+      throw new InternalServerError(
+        `Error while retrieving credential definition with id ${credentialDefinitionId}`,
+        error
+      )
+    }
+  }
+
+  @Post()
+  async createCredentialDefinition(
+    @BodyParams('data')
+    data: {
+      tag: string
+      support_revocation: boolean
+      schema_id: string
+    }
+  ): Promise<{
+    credential_definition_id: string
+    credential_definition: ReturnedCredentialDefinition
+  }> {
+
+    // Check locally if credential definition already exists
+    const credentialDefinitionRepository = this.agent.dependencyManager.resolve(AnonCredsCredentialDefinitionRepository)
+    const [credentialDefinitionRecord] = await credentialDefinitionRepository.findByQuery(this.agent.context, { schemaId: data.schema_id, tag: data.tag })
+    if (credentialDefinitionRecord) {
+
+      return {
+        credential_definition_id: credentialDefinitionRecord.credentialDefinitionId,
+        credential_definition: { ...credentialDefinitionRecord.credentialDefinition, id: credentialDefinitionRecord.credentialDefinitionId },
+      }
+    }
+
+    // TODO: handle schema not found exception
+    try {
+
+      const anoncredsApi = this.agent.dependencyManager.resolve(AnonCredsApi)
+
+      const schema = await anoncredsApi.getSchema(data.schema_id)
+
+      const publicDidInfoRecord = await this.agent.genericRecords.findById('PUBLIC_DID_INFO')
+    
+      if (!publicDidInfoRecord) {
+        throw new Error('Agent does not have any public did')
+      }
+  
+      //const issuerId = (publicDidInfoRecord.content.didInfo as unknown as DidInfo).did
+      const prefix = 'did:indy:bcovrin:test:'; //TODO compile the prefix from variables.
+      const id = (publicDidInfoRecord.content.didInfo as unknown as DidInfo).did
+      const issuerId = `${prefix}${id}`;
+      const { credentialDefinitionState } = await anoncredsApi.registerCredentialDefinition({ 
+        credentialDefinition: {
+          issuerId,
+          schemaId: schema.schemaId,
+          tag: data.tag,
+        }, 
+        //options: { supportRevocation: false, didIndyNamespace: 'main-pool'}}) 
+        options: { supportRevocation: false, indyNamespace: 'bcovrin:test'}}) 
+
+      if (!credentialDefinitionState.credentialDefinition || !credentialDefinitionState.credentialDefinitionId) {
+        throw new Error()
+      }
+      
+      return {
+        credential_definition_id: credentialDefinitionState.credentialDefinitionId,
+        credential_definition: { ...credentialDefinitionState.credentialDefinition, id: credentialDefinitionState.credentialDefinitionId },
+      }
+    } catch (error: any) {
+      throw new InternalServerError(`Error registering credential definition: ${error.message}`, error)
+    }
+  }
+}
+
+interface ReturnedCredentialDefinition extends AnonCredsCredentialDefinition {
+  id: string
+}

aries-backchannels/credo-ts/server/src/controllers/OutOfBandController.ts
@@ -1,0 +1,172 @@
+/* import { Controller, Get, PathParams, Post, BodyParams } from '@tsed/common'
+import { NotFound } from '@tsed/exceptions'
+import {
+  OutOfBandRecord,
+  OutOfBandInvitation,
+  ConnectionInvitationMessage,
+  ConnectionRecord,
+  JsonTransformer,
+  AgentConfig,
+  OutOfBandEventTypes,
+  OutOfBandStateChangedEvent,
+  OutOfBandState,
+  CredoError,
+  DidExchangeState,
+} from '@credo-ts/core'
+
+import { convertToNewInvitation } from '@credo-ts/core/build/modules/oob/helpers'
+import { filter, firstValueFrom, ReplaySubject, tap, timeout } from 'rxjs'
+import { BaseController } from '../BaseController'
+import { TestHarnessConfig } from '../TestHarnessConfig'
+import { ConnectionUtils } from '../utils/ConnectionUtils'
+
+@Controller('/agent/command/oob')
+export class OutOfBandController extends BaseController {
+  private subject = new ReplaySubject<OutOfBandStateChangedEvent>()
+
+  public constructor(testHarnessConfig: TestHarnessConfig) {
+    super(testHarnessConfig)
+  }
+
+  onStartup = () => {
+    this.subject = new ReplaySubject<OutOfBandStateChangedEvent>()
+    // Catch all events in replay subject for later use
+    this.agent.events
+      .observable<OutOfBandStateChangedEvent>(OutOfBandEventTypes.OutOfBandStateChanged)
+      .subscribe(this.subject)
+  }
+
+  @Get('/:id')
+  async getConnectionById(@PathParams('id') id: string) {
+    const connection = await ConnectionUtils.getConnectionByConnectionIdOrOutOfBandId(this.agent, id)
+
+    if (!connection) throw new NotFound(`Connection with id ${id} not found`)
+
+    return this.mapConnection(connection)
+  }
+
+  @Get('/')
+  async getAllConnections() {
+    const connections = await this.agent.connections.getAll()
+
+    return connections.map((conn) => this.mapConnection(conn))
+  }
+
+  @Post('/create-invitation')
+  async createInvitation(@BodyParams('data') data?: { mediator_connection_id?: string }) {
+    const mediatorId = await this.mediatorIdFromMediatorConnectionId(data?.mediator_connection_id)
+
+    const routing = await this.agent.mediationRecipient.getRouting({
+      mediatorId,
+    })
+
+    const outOfBandRecord = await this.agent.oob.createInvitation({
+      routing,
+      // Needed to complete connection: https://github.com/hyperledger/aries-framework-javascript/issues/668
+      // TODO probably don't need this anymore.
+      autoAcceptConnection: true,
+    })
+
+    const config = this.agent.dependencyManager.resolve(AgentConfig)
+    const invitation = outOfBandRecord.outOfBandInvitation
+    const invitationJson = invitation.toJSON({ useDidSovPrefixWhereAllowed: config.useDidSovPrefixWhereAllowed })
+
+    return {
+      state: OutOfBandState.Initial,
+      // This should be the connection id. However, the connection id is not available until a request is received.
+      // We can just use the oob invitation I guess.
+      connection_id: outOfBandRecord.id,
+      invitation: invitationJson,
+    }
+  }
+
+  @Post('/receive-invitation')
+  async receiveInvitation(@BodyParams('data') data: Record<string, unknown> & { mediator_connection_id?: string }) {
+    const { mediator_connection_id, ...invitationJson } = data
+
+    const mediatorId = await this.mediatorIdFromMediatorConnectionId(mediator_connection_id)
+
+    const routing = await this.agent.mediationRecipient.getRouting({
+      mediatorId,
+    })
+
+    const oobInvitation = convertToNewInvitation(JsonTransformer.fromJSON(invitationJson, ConnectionInvitationMessage))
+    const { outOfBandRecord, connectionRecord } = await this.agent.oob.receiveInvitation(oobInvitation, {
+      routing,
+      // Needed to complete connection: https://github.com/hyperledger/aries-framework-javascript/issues/668
+      autoAcceptConnection: true,
+      autoAcceptInvitation: true,
+    })
+
+    this.agent.config.logger.debug('OutOfBandController.receiveInvitation: outOfBandRecord.id: ', outOfBandRecord)
+
+    if (!connectionRecord) {
+      throw new CredoError('Processing invitation did not result in a out of band record')
+    }
+
+    // return this.mapConnection(outOfBandRecord)
+    return this.mapConnection(connectionRecord)
+    
+  }
+
+  @Post('/accept-invitation')
+  async acceptInvitation(@BodyParams('id') id: string) {
+    const connection = await ConnectionUtils.getConnectionByConnectionIdOrOutOfBandId(this.agent, id)
+
+    return this.mapConnection(connection)
+  }
+
+  @Post('/accept-request')
+  async acceptRequest(@BodyParams('id') id: string) {
+    // possible here that the request hasn't finished processing yet
+    await this.waitForState(id, DidExchangeState.RequestReceived)
+
+    const connection = await ConnectionUtils.getConnectionByConnectionIdOrOutOfBandId(this.agent, id)
+
+    return this.mapConnection(connection)
+  }
+
+  @Post('/send-ping')
+  async sendPing(
+    @BodyParams('id') id: string,
+    // For now we ignore data. This could contain a comment property,
+    // AFJ doesn't support passing it for now
+    @BodyParams('data') data: any
+  ) {
+    const connection = await ConnectionUtils.getConnectionByConnectionIdOrOutOfBandId(this.agent, id)
+
+    if (!connection) throw new NotFound(`Connection with id ${id} not found`)
+
+    return this.mapConnection(connection)
+  }
+
+  private async mediatorIdFromMediatorConnectionId(mediatorConnectionId?: string): Promise<string | undefined> {
+    if (!mediatorConnectionId) return undefined
+
+    // Find mediator id if mediator connection id is provided
+    const mediator = await this.agent.mediationRecipient.findByConnectionId(mediatorConnectionId)
+
+    return mediator?.id
+  }
+
+  private async waitForState(id: string, state: DidExchangeState) {
+    return await firstValueFrom(
+      this.subject.pipe(
+        filter((c) => c.payload.connectionRecord.id === id ),
+        filter((c) => c.payload.connectionRecord.state === state),
+        timeout(20000)
+      )
+    )
+  }
+
+  private mapConnection(connection: ConnectionRecord) {
+    return {
+      // If we use auto accept, we can't include the state as we will move quicker than the calls in the test harness. This will
+      // make verification fail. The test harness recognizes the 'N/A' state.
+      state: connection.state === DidExchangeState.Completed ? connection.rfc0160State : 'N/A',
+      connection_id: connection.id,
+      connection,
+    }
+  }
+}
+ */